0d6b52667857282abef87892e2ebf72946ba4495
pradyumna-prog <pradyumnatripathi99@gmail.com>
2020-07-25 18:20:40 +0530

    added technical strength section

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function addTechnicalSkill(listItemId){
+let listItemId = 0;
+
+function addTechnicalSkill(){
     li = document.createElement('li');
     key = document.getElementById("technicalKey").value;
     values = document.getElementById("technicalValue").value;
@@ -9,11 +11,20 @@ function addTechnicalSkill(listItemId){
         alert('Plese fill values');
     }
     else{
-        li.innerHTML = '◾ ' + key + " : " + values;
+        document.getElementById("technicalKey").value = "";
+        document.getElementById("technicalValue").value = "";
+
+        span = document.createElement("span");
+        span.innerHTML = '◾ ' + key + " : " + values;
+        li.setAttribute('id',listItemId);
+        span.setAttribute('contenteditable', '');
+        li.appendChild(span)
 
         btnRemove = document.createElement('button');
         btnRemove.innerHTML = 'X';
+        btnRemove.setAttribute('id',listItemId++);
         btnRemove.addEventListener('click', btnRemoveHandler);
+        li.appendChild(btnRemove);
 
         ul = document.querySelector("#technicalSkills");
         ul.style.marginTop = '5px';
@@ -21,6 +32,6 @@ function addTechnicalSkill(listItemId){
     }
 }
 
-function btnRemoveHandler(){
-    
+function btnRemoveHandler(event){
+    document.getElementById(event['target']['id']).remove();
 }
