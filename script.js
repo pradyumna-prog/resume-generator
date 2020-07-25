c3a730a7bbf6179e2a39f9e76c57dda6eb68c9e4
pradyumna-prog <pradyumnatripathi99@gmail.com>
2020-07-25 21:24:11 +0530

    design complete

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-let listItemId = 0;
+let skillId = 'T0', projectId = 'P1', strengthId = 'S0', workshopId = 'W0', achievementId = 'A0';
 
 addTechnicalSkill = () => {
-    const listItem = 
     li = document.createElement('li');
+    li.setAttribute('id',skillId);
+
     key = document.getElementById("technicalKey").value;
     values = document.getElementById("technicalValue").value;
-    if(key == ''){
-        alert('Plese fill key');
-    }
-    else if(values == ''){
-        alert('Plese fill values');
-    }
-    else{
-        document.getElementById("technicalKey").value = "";
-        document.getElementById("technicalValue").value = "";
 
-        span = document.createElement("span");
-        span.innerHTML = '◾ ' + key + " : " + values;
-        li.setAttribute('id',listItemId);
-        span.setAttribute('contenteditable', '');
-        li.appendChild(span)
+    document.getElementById("technicalKey").value = "";
+    document.getElementById("technicalValue").value = "";
 
-        btnRemove = document.createElement('button');
-        btnRemove.innerHTML = 'X';
-        btnRemove.setAttribute('id',listItemId++);
-        btnRemove.addEventListener('click', btnRemoveHandler);
-        li.appendChild(btnRemove);
+    span = document.createElement("span");
+    span.innerHTML = '◾ ' + key + " : " + values;
+    span.setAttribute('contenteditable', '');
+    li.appendChild(span)
 
-        ul = document.querySelector("#technicalSkills");
-        ul.style.marginTop = '5px';
-        ul.appendChild(li);
-    }
+    btnRemove = document.createElement('button');
+    btnRemove.innerHTML = 'X';
+    btnRemove.setAttribute('id',skillId);
+    btnRemove.setAttribute('class', 'btnRemove');
+    btnRemove.addEventListener('click', btnRemoveHandler);
+    li.appendChild(btnRemove);
+
+    ul = document.querySelector("#technicalSkills");
+    ul.style.marginTop = '5px';
+    ul.appendChild(li);
+
+    skillId = incrementId("technical", skillId);
 }
 
 function btnRemoveHandler(event){
@@ -38,5 +34,88 @@ function btnRemoveHandler(event){
 }
 
 addProject = () => {
-    console.log("called")
+    article = document.createElement('article');
+    article.setAttribute('id', projectId);
+
+    btnRemove = document.createElement('button');
+    btnRemove.innerHTML = 'X';
+    btnRemove.setAttribute('id',projectId);
+    btnRemove.setAttribute('class', 'btnRemove');
+    btnRemove.addEventListener('click', btnRemoveHandler);
+
+    h2 = document.createElement('h2');
+    h2.setAttribute('contenteditable','');
+    h2.innerHTML = "Project Title";
+    
+    spanT = document.createElement('span');
+    spanT.setAttribute('contenteditable','');
+    spanT.innerHTML = 'add';
+    spanR = document.createElement('span');
+    spanR.setAttribute('contenteditable','');
+    spanR.innerHTML = 'add';
+    spanD = document.createElement('span');
+    spanD.setAttribute('contenteditable','');
+    spanD.innerHTML = 'add';
+
+    subdetailPara = document.createElement('p');
+    subdetailPara.setAttribute('class', 'subDetails');
+    subdetailPara.innerHTML = "Technologies :";
+    subdetailPara.appendChild(spanT);
+    subdetailPara.innerHTML += " | Role :";
+    subdetailPara.appendChild(spanR);
+    subdetailPara.innerHTML += " | Duration :";
+    subdetailPara.appendChild(spanD);
+
+    descripton = document.createElement('p');
+    descripton.setAttribute('contenteditable','');
+    descripton.innerHTML = "Add description";
+
+    article.appendChild(h2);
+    article.appendChild(btnRemove);
+    article.appendChild(subdetailPara);
+    article.appendChild(descripton);
+
+    document.getElementById("projectDetails").appendChild(article);
+
+    projectId = incrementId('project', projectId);
+}
+
+function addSWC(type) {
+    if(type == "strengths") id = strengthId;
+    else if(type == "workshops") id = workshopId;
+    else if(type == "achievements") id = achievementId;
+
+    li = document.createElement('li');
+    li.setAttribute('id',id);
+
+    inputValue = document.getElementById(type+"Value").value;
+    document.getElementById(type+"Value").value = "";
+
+    span = document.createElement("span");
+    span.innerHTML = '◾ ' + inputValue;
+    span.setAttribute('contenteditable', '');
+    li.appendChild(span)
+
+    btnRemove = document.createElement('button');
+    btnRemove.innerHTML = 'X';
+    btnRemove.setAttribute('id',id);
+    btnRemove.setAttribute('class', 'btnRemove');
+    btnRemove.addEventListener('click', btnRemoveHandler);
+    li.appendChild(btnRemove);
+
+    ul = document.getElementById(type);
+    ul.style.marginTop = '5px';
+    ul.appendChild(li);
+
+    incrementId(type, id);
+}
+
+function incrementId(type, strId){
+
+    id = parseInt(strId.slice(1, strId.length), 10);
+    
+    if(type == "strengths") strengthId = strId[0]+ ++id;
+    else if(type == "workshops") workshopId = strId[0]+ ++id;
+    else if(type == "achievements") achievementId = strId[0]+ ++id;
+    else return strId[0]+ ++id ;
 }
